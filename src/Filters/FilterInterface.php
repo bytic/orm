6cea62c2937796c2c937c30d7d2182b593bc59eb
Gabriel Solomon <hello@gabrielsolomon.ro>
2018-02-01 23:18:42 +0200

    initial commit

---
+++ src/Filters/FilterInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Nip\Records\Filters;
+
+interface FilterInterface
+{
+    public function getName();
+
+    public function getValue();
+
+    public function getManager();
+
+    public function setManager($field);
+}
