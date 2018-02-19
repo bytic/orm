5d47ba9871d2e2444fe3566ba78e1272fce9e28a
Gabriel Solomon <hello@gabrielsolomon.ro>
2018-02-19 17:21:12 +0200

    fix pivotTable to legacy name

---
+++ tests/Relations/HasAndBelongsToManyTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Nip\Records\Tests\Relations;
+
+use Nip\Records\RecordManager;
+use Nip\Records\Relations\HasAndBelongsToMany;
+
+/**
+ * Class HasAndBelongsToManyTest
+ * @package Nip\Records\Tests\Relations
+ */
+class HasAndBelongsToManyTest extends \Nip\Records\Tests\AbstractTest
+{
+
+    public function testTableNameGeneration()
+    {
+        $tableA = new RecordManager();
+        $tableA->setTable('tableA');
+
+        $tableB = new RecordManager();
+        $tableB->setTable('tableB');
+
+        $relation = new HasAndBelongsToMany();
+        $relation->setManager($tableA);
+        $relation->setWith($tableB);
+        self::assertEquals('tableA-tableB', $relation->getTable());
+
+        $relation = new HasAndBelongsToMany();
+        $relation->setManager($tableB);
+        $relation->setWith($tableA);
+        self::assertEquals('tableA-tableB', $relation->getTable());
+    }
+}
