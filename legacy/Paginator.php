d8db7db3076cf9adf2e15a4d2c00a07ad3e345cb
Gabriel Solomon <hello@gabrielsolomon.ro>
2018-02-25 20:27:24 +0200

    initial update paginator to use separate count query

---
+++ legacy/Paginator.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Class Nip_Record_Paginator
+ *
+ * @deprecated Use new \Nip\Records\Navigator\Pagination\Paginator
+ */
+class Nip_Record_Paginator extends \Nip\Records\Navigator\Pagination\AbstractPaginator
+{
+}
---
+++ src/Navigator/Pagination/Paginator.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Nip\Records\Navigator\Pagination;
+
+/**
+ * Class Paginator
+ * @package Nip\Records\Navigator\Pagination
+ */
+class Paginator extends AbstractPaginator
+{
+}
