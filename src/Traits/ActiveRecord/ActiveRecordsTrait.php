92e551114b74789ffdbbc77250b5bbd608d95b16
Gabriel Solomon <hello@gabrielsolomon.ro>
2019-01-06 17:42:37 +0200

    create new searchable trait

--- src/Traits/ActiveRecord/ActiveRecordsTrait.php
+++ src/Traits/ActiveRecord/ActiveRecordsTrait.php
@@ -8,10 +8,10 @@ use Nip\Database\Query\Insert as InsertQuery;
 use Nip\Database\Query\Select as SelectQuery;
 use Nip\Database\Query\Update as UpdateQuery;
 use Nip\Database\Result;
-use Nip\Paginator;
 use Nip\Records\AbstractModels\Record;
 use Nip\Records\Collections\Collection as RecordCollection;
 use Nip\Records\Traits\HasDatabase\HasDatabaseRecordsTrait;
+use Nip\Records\Traits\Searchable\SearchableRecordsTrait;
 use Nip\Records\Traits\TableStructure\TableStructureRecordsTrait;
 use Nip\Records\Traits\Unique\RecordsTrait as UniqueRecordsTrait;
 use Nip\Records\Traits\HasForeignKey\RecordsTrait as HasForeignKeyTrait;
@@ -26,6 +26,7 @@ trait ActiveRecordsTrait
     use UniqueRecordsTrait;
     use HasForeignKeyTrait;
     use HasPrimaryKeyTrait;
+    use SearchableRecordsTrait;
     use TableStructureRecordsTrait;
     use HasDatabaseRecordsTrait;
 
@@ -56,7 +57,6 @@ trait ActiveRecordsTrait
 
         return $query;
     }
-
 
     /**
      * @return string
@@ -264,29 +264,6 @@ trait ActiveRecordsTrait
     }
 
     /**
-     * Returns paginated results
-     * @param Paginator $paginator
-     * @param array $params
-     * @return mixed
-     */
-    public function paginate(Paginator $paginator, $params = [])
-    {
-        $query = $this->paramsToQuery($params);
-
-        $countQuery = $this->getDB()->newSelect();
-        $countQuery->count(['*', 'count']);
-        $countQuery->from([$query, 'tbl']);
-        $results = $countQuery->execute()->fetchResults();
-        $count = $results[0]['count'];
-
-        $paginator->setCount($count);
-
-        $params['limit'] = $paginator->getLimits();
-
-        return $this->findByParams($params);
-    }
-
-    /**
      * @param array $params
      * @return SelectQuery
      */
@@ -298,85 +275,6 @@ trait ActiveRecordsTrait
         $query->addParams($params);
 
         return $query;
-    }
-
-    /**
-     * @param array $params
-     */
-    protected function injectParams(&$params = [])
-    {
-    }
-
-    /**
-     * Checks the registry before fetching from the database
-     * @param mixed $primary
-     * @return Record
-     */
-    public function findOne($primary)
-    {
-        $item = $this->getRegistry()->get($primary);
-        if (!$item) {
-            $all = $this->getRegistry()->get("all");
-            if ($all) {
-                $item = $all[$primary];
-            }
-            if (!$item) {
-                $params['where'][] = ["`{$this->getTable()}`.`{$this->getPrimaryKey()}` = ?", $primary];
-                $item = $this->findOneByParams($params);
-                if ($item) {
-                    $this->getRegistry()->set($primary, $item);
-                }
-
-                return $item;
-            }
-        }
-
-        return $item;
-    }
-
-    /**
-     * @param Query $query
-     * @param array $params
-     * @return bool
-     */
-    public function findOneByQuery($query, $params = [])
-    {
-        $query->limit(1);
-        $return = $this->findByQuery($query, $params);
-        if (count($return) > 0) {
-            return $return->rewind();
-        }
-
-        return null;
-    }
-
-    /**
-     * @param Query $query
-     * @param array $params
-     * @return RecordCollection
-     */
-    public function findByQuery($query, $params = [])
-    {
-        $return = $this->newCollection();
-
-        $results = $this->getDB()->execute($query);
-        if ($results->numRows() > 0) {
-            $pk = $this->getPrimaryKey();
-            /** @noinspection PhpAssignmentInConditionInspection */
-            while ($row = $results->fetchResult()) {
-                $item = $this->getNew($row);
-                if (is_string($pk)) {
-                    $this->getRegistry()->set($item->getPrimaryKey(), $item);
-                }
-                if (isset($params['indexKey']) && !empty($params['indexKey'])) {
-                    $return->add($item, $params['indexKey']);
-                } else {
-                    $return->add($item);
-                }
-            }
-        }
-
-        return $return;
     }
 
     /**
