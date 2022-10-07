0fcf897f14b99a8f2b62f74f7bb362745db51f39
Gabriel Solomon <hello@gabrielsolomon.ro>
2022-10-07 21:52:18 +0300

    fix: url generation withought model

--- src/Traits/HasUrl/HasUrlRecordManagerTrait.php
+++ src/Traits/HasUrl/HasUrlRecordManagerTrait.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Nip\Records\Traits\HasUrl;
 
 /**
@@ -19,8 +21,8 @@ trait HasUrlRecordManagerTrait
     {
         if (substr($name, 0, 3) == "get" && substr($name, -3) == "URL") {
             $action = substr($name, 3, -3);
-            $params = isset($arguments[0]) ? $arguments[0] : [];
-            $module = isset($arguments[1]) ? $arguments[1] : null;
+            $params = $arguments[0] ?? [];
+            $module = $arguments[1] ?? null;
 
             return $this->compileURL($action, $params, $module);
         }
@@ -56,8 +58,11 @@ trait HasUrlRecordManagerTrait
         $params['action'] = inflector()->unclassify($params['action']);
         $params['action'] = ($params['action'] == 'index') ? null : $params['action'];
 
-        $params['controller'] = $controller ? $controller : $this->getController();
-        $params['module'] = $module ? $module : request()->getModuleName();
+        $params['controller'] = $controller ?: $this->getController();
+        $params['module'] = $module ?: request()->getModuleName();
+        if (empty($params['module'])) {
+            $params['module'] = config('mvc.defaults.module');
+        }
 
         $routeName = $params['module'] . '.' . $params['controller'] . '.' . $params['action'];
         if (app()->get('router')->hasRoute($routeName)) {
